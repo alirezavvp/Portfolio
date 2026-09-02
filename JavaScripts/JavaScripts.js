e5f5851428d2961411468904d6d7135841b674b5
alirezavvp <alirezavalaei2@gmail.com>
2026-09-02 11:58:16 +0330

    mobile menu updated

--- JavaScripts/JavaScripts.js
+++ JavaScripts/JavaScripts.js
@@ -4,8 +4,8 @@ const siteContent = document.querySelector("#site-content");
 
 // هنگام اسکرول کردن منوی اصلی تغییر ظاهر میدهد
 window.addEventListener("scroll", function () {
-  const header = document.getElementById("main-header");
-  if (window.scrollY > 60) {
+  const header = document.querySelector("#main-header");
+  if (window.scrollY > 1) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
@@ -14,20 +14,18 @@ window.addEventListener("scroll", function () {
 
 // باز کردن منو در موبایل و قفل شدن اسکرول و تاریک شدن
 document.querySelector("#toggle-menu > svg").addEventListener("click", () => {
+  document.body.classList.toggle("no-scroll");
   mobileMenu.classList.toggle("aside");
   document.getElementById("overlay").classList.toggle("show");
   siteContent.classList.add("margin");
-  // siteContent.style.marginRight = "210px";
-  document.body.classList.toggle("no-scroll");
 });
 
 // بستن منو در موبایل
 document.querySelector("#close-menu > svg").addEventListener("click", () => {
+  document.body.classList.toggle("no-scroll");
   mobileMenu.classList.remove("aside");
   document.getElementById("overlay").classList.remove("show");
   siteContent.classList.remove("margin");
-  // siteContent.style.marginRight = "0";
-  document.body.classList.remove("no-scroll");
 });
 
 // هنگام اسکرول کردن محتوا به شکل مخصوصی ظاهر میشود
